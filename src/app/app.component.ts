9aac58b7d768e3eb0d2765886701390daa164c04
luancaike <lmatos@brasiliano.com.br>
2020-05-11 17:17:13 -0300

    Added Component Validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FormBuilder]
 })
 export class AppComponent {
   title = 'forms-generator';
+  profileForm = this.fb.group({
+    username: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+    password: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+    savelogin: [null, [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.profileForm);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { InputValidatorComponent } from './components/input-validator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputValidatorComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
