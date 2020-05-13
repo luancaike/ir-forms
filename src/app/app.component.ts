74330b3131062e23c4e2986fc3ff40f586f2ef34
luancaike <lmatos@brasiliano.com.br>
2020-05-13 18:19:18 -0300

    Added Input Form separated of form and dynamic render components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [FormBuilder],
 })
-export class AppComponent {
-  title = 'forms-generator';
-  profileForm = this.fb.group({
-    username: [
-      null,
-      [Validators.required, Validators.minLength(2), Validators.maxLength(10)],
-    ],
-    password: [
-      null,
-      [Validators.required, Validators.minLength(2), Validators.maxLength(10)],
-    ],
-    savelogin: [null, [Validators.required]],
-  });
-
-  constructor(private fb: FormBuilder) {}
-
-  onSubmit(): void {
-    // TODO: Use EventEmitter with form value
-    console.log(this.profileForm);
-  }
-}
+export class AppComponent {}
--- src/app/components/inputs/text-input.component.ts
+++ src/app/components/inputs/text-input.component.ts
@@ -4,23 +4,25 @@ import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'ir-text-input',
   template: `
-    <div class="form-group" [formGroup]="formRoot">
-      <label [for]="inputKey">{{ label }}</label>
-      <input
-        [class.is-invalid]="
-          formRoot.controls[inputKey].invalid &&
-          (formRoot.controls[inputKey].dirty ||
-            formRoot.controls[inputKey].touched)
-        "
-        class="form-control"
-        [formControlName]="inputKey"
-        [id]="inputKey"
-        [placeholder]="placeholder"
-      />
-      <ir-input-validator
-        [formChild]="formRoot.controls[inputKey]"
-      ></ir-input-validator>
-    </div>
+    <ng-template [ngIf]="formRoot">
+      <div class="form-group" [formGroup]="formRoot">
+        <label [for]="inputKey">{{ label }}</label>
+        <input
+          [class.is-invalid]="
+            formRoot.controls[inputKey].invalid &&
+            (formRoot.controls[inputKey].dirty ||
+              formRoot.controls[inputKey].touched)
+          "
+          class="form-control"
+          [formControlName]="inputKey"
+          [id]="inputKey"
+          [placeholder]="placeholder"
+        />
+        <ir-input-validator
+          [formChild]="formRoot.controls[inputKey]"
+        ></ir-input-validator>
+      </div>
+    </ng-template>
   `,
   styles: [],
 })
